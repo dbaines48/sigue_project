d175bb00cead8f20d02c5941503baf2edb041934
Alexander Taborda Acosta <alextab93@gmail.com>
2015-09-06 18:49:06 -0500

    add custom.css

--- js/app.js
+++ js/app.js
@@ -4,6 +4,14 @@ angular.module('SigueProject', ['ngRoutes'])
     .when('/', {
       redirectTo: '/home'
     })
+    .when('/explore', {
+      templateUrl: 'templates/pages/explore/index.html',
+      controller: 'ExploreIndexController'
+    })
+    .when('/explorer/:id', {
+      templateUrl: 'templates/pages/explore/index.html',
+      controller: 'ExploreShowController'
+    })
     .otherwise({
       redirectTo: '/home'
     });
